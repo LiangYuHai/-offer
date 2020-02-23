e7db263dea356dfb0b19923cb8fdfbe3cf79f4e4
lyh131201 <lyh20180204@163.com>
2020-02-23 10:51:04 +0800

    add 面试题 ~ 49

---
+++ atoi.cpp
@@ -0,0 +1,33 @@
+//49 atoi
+// Created by Administrator on 2020/2/18 0018.
+//
+
+#include <iostream>
+using namespace std;
+
+bool InputInvalid = true;
+long long atoi(char* str){
+    if(!str || *str == '\0') {
+        return 0;
+    }
+    bool minus = false;
+    if(*str == '+') str++;
+    else if(*str == '-'){
+        minus = true;
+        ++str;
+    }else{
+        ++str;
+    }
+    if(*str == '\0'){
+        return 0;
+    }
+    long long number = 0;
+    unsigned short int flag = minus ? 1 : -1;
+    while(*str != '\0'){
+        if(*str < '0' || *str > '9' || (minus && number > 0x7fffffff) || (!minus && number < 0x80000000)) return 0;
+        number = 10*number + flag*(*str - '0');
+    }
+    InputInvalid = false;
+    return minus ? -number : number;
+}
+
